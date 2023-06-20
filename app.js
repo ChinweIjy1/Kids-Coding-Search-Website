68fef0badf47c61dce0f64426d3a4f8e229cfa8a
Adama <adamamuhammad01@gmail.com>
2023-06-20 07:56:36 -0700

    update

---
+++ app.js
@@ -0,0 +1,5 @@
+let searchButton = document.querySelector('.srch-btn');
+let LangButton = document.querySelector('.btn');
+let closeButton = document.querySelector('#close');
+searchButton.addEventListener('click', getPL);
+
